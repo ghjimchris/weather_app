70c1f3112950bcaee5c57fcb86c9e0677a08743b
ghjimchris <jimrajchris@gmail.com>
2022-12-17 14:58:01 +0000

    weather app - VUE

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+import { RouterView } from 'vue-router';
+<template>
+  <div class="flex-col min-h-screen font-Roboto bg-wheather-primary">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="sticky top-0 shadow-lg bg-wheather-primary">
+    <nav
+      class="container flex flex-col items-center gap-4 py-6 text-black sm:flex"
+    ></nav>
+    <div class="grid grid-cols-3 gap-2 mx-2 bg-blue-800 rounded-lg">Hl</div>
+    <div class="p-6 bg-green-600 border-4 border-green-300 border-dashed"></div>
+  </header>
+</template>
+
+<script setup></script>
